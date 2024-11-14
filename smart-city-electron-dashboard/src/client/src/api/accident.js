543a8509f3a1de1f0667341c01c8ecab0c8f1fe6
Yukta Mehta <mehtayukta039@gmail.com>
2024-11-14 12:27:03 -0800

    removed commented code

--- smart-city-electron-dashboard/src/client/src/api/accident.js
+++ smart-city-electron-dashboard/src/client/src/api/accident.js
@@ -1,23 +1,3 @@
-// import axios from 'axios';
-// import config from '../config.json'; // Import JSON config
-
-// // Get the appropriate BASE_URL based on environment
-// const env = process.env.NODE_ENV || 'development';
-// const BASE_URL = config[env].BASE_URL;
-
-// export const getAccidents = async () => {
-//     try {
-//      console.log("In accident.js")
-//       // return true;
-//       //const response = await axios.get(`${BASE_URL}/accidentsdata`);
-//       console.log(`Fetching from ${BASE_URL}/accidentsdata`);
-//       const response = await axios.get(`${BASE_URL}/accidentsdata`);
-
-//       return response.data;
-//     } catch (error) {
-//       throw new Error(error.response.data.message);
-//     }
-//   };
 import axios from 'axios';
 import config from '../config.json'; // Import JSON config
 
@@ -33,16 +13,6 @@ export const getAccidents = async () => {
         console.log(`Fetching from ${BASE_URL}/accidentsdata`);
         const response = await axios.get(`${BASE_URL}/accidentsdata`);
   
-        return response.data;
-      } catch (error) {
-        throw new Error(error.response.data.message);
-      }
-    };
-
-export const getDroneStations = async () => {
-      try {
-        // return true;
-        const response = await axios.get(`${BASE_URL}/dronesStations`);
         return response.data;
       } catch (error) {
         throw new Error(error.response.data.message);
